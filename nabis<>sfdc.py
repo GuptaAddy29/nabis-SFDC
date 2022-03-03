236e11e0e9143545d0b4223262e28ca2cc6861c9
Adarsh Gupta <30989741+GuptaAddy29@users.noreply.github.com>
2022-03-03 11:21:29 -0800

    Add files via upload
    NabisSfdcmappinggsheetconfig

---
+++ nabis<>sfdc.py
@@ -0,0 +1,9 @@
+from task_configs.gsheets import GsheetConfig
+
+gsheet_config = GsheetConfig(
+    file_name='Nabis<>SFDC',
+    file_id='1nr28ohphotFneIvM_UwY9lOdK76iOHcpqqLUtuEecao',
+    table_name='NabisSfdcMapping',
+    sheet_name='Data',
+    range='A:G',
+)
